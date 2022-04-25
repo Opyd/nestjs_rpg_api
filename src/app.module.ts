c664987ef6300d0d44e04911f2ed97f5ad145e09
Daniel <danielopyd99@gmail.com>
2022-04-25 18:43:02 +0200

    mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://pajrat:test1234@testing-database.lto2w.mongodb.net/tes?authSource=admin&replicaSet=atlas-nwety8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
