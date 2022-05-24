4258a7793a789d3d3f2859a81fce5c87d81596af
Szymekk <skucharek2@gmail.com>
2022-05-25 00:05:11 +0200

    campaigns done

--- src/campaigns/campaigns.module.ts
+++ src/campaigns/campaigns.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CampaignsService } from './campaigns.service';
 import { CampaignsController } from './campaigns.controller';
+import { Campaign, CampaignSchema } from './schemas/campaign.schema';
+import { Place, PlaceSchema } from 'src/places/schemas/places.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Character,
+  CharacterSchema,
+} from 'src/characters/schemas/character.schema';
+import { EventSchema } from 'src/events/schemas/event.schema';
+import { Item, ItemSchema } from 'src/items/schemas/item.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Campaign.name, schema: CampaignSchema },
+      { name: Place.name, schema: PlaceSchema },
+      { name: Character.name, schema: CharacterSchema },
+      { name: Event.name, schema: EventSchema },
+      { name: Item.name, schema: ItemSchema },
+    ]),
+  ],
   controllers: [CampaignsController],
-  providers: [CampaignsService]
+  providers: [CampaignsService],
 })
 export class CampaignsModule {}
--- src/campaigns/campaigns.service.ts
+++ src/campaigns/campaigns.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Character } from 'src/characters/schemas/character.schema';
+import { Item } from 'src/items/schemas/item.schema';
+import { Place } from 'src/places/schemas/places.schema';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
 import { UpdateCampaignDto } from './dto/update-campaign.dto';
+import { Campaign, CampaignDocument } from './schemas/campaign.schema';
 
 @Injectable()
 export class CampaignsService {
-  create(createCampaignDto: CreateCampaignDto) {
-    return 'This action adds a new campaign';
+  constructor(
+    @InjectModel(Campaign.name)
+    private campaignModel: Model<CampaignDocument>,
+    @InjectModel(Place.name) public placeModel: Model<Place>,
+    @InjectModel(Character.name) public characterModel: Model<Character>,
+    @InjectModel(Event.name) public eventModel: Model<Event>,
+    @InjectModel(Item.name) public itemModel: Model<Item>,
+  ) {}
+  async create(createCampaignDto: CreateCampaignDto) {
+    return new this.campaignModel(createCampaignDto).save();
   }
 
   findAll() {
-    return `This action returns all campaigns`;
+    return this.campaignModel
+      .find()
+      .populate(['locations', 'characters', 'events', 'items'])
+      .exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} campaign`;
+  findOne(id: string) {
+    return this.campaignModel.findById(id).exec();
   }
 
-  update(id: number, updateCampaignDto: UpdateCampaignDto) {
-    return `This action updates a #${id} campaign`;
+  update(id: string, updateCampaignDto: UpdateCampaignDto) {
+    return this.campaignModel.findByIdAndUpdate(id, updateCampaignDto, {
+      new: true,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} campaign`;
+  remove(id: string) {
+    return this.campaignModel.findByIdAndDelete(id).exec();
   }
 }
